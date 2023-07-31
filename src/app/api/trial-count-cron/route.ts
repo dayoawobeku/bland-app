6374355bfbe19e25241df70cdc981f68b4ed3337
Dayo <dayo@dojah.io>
2023-07-31 16:36:10 +0100

    removed caching from GET request

---
+++ src/app/api/trial-count-cron/route.ts
@@ -0,0 +1,52 @@
+import {NextResponse} from 'next/server';
+import {getFirestore} from 'firebase-admin/firestore';
+import {createFirebaseAdminApp} from '@/helpers/firebase-admin';
+
+async function initializeAdmin() {
+  const params = {
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
+    clientEmail: process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL as string,
+    privateKey: process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY as string,
+  };
+
+  return createFirebaseAdminApp(params);
+}
+
+export const revalidate = 0;
+
+export async function GET() {
+  const {success} = await updateTrialCount();
+
+  if (success) {
+    return NextResponse.json({
+      success: true,
+      message: 'Trial count updated to 3 for all users',
+    });
+  } else {
+    return NextResponse.json({
+      success: false,
+      message: 'Error updating trial count',
+    });
+  }
+}
+
+async function updateTrialCount() {
+  try {
+    await initializeAdmin();
+    const firestore = getFirestore();
+    const usersSnapshot = await firestore.collection('users').get();
+
+    const updates = usersSnapshot.docs.map(doc => {
+      const userRef = doc.ref;
+      return userRef.update({trialCount: 3});
+    });
+
+    await Promise.all(updates);
+    console.log('Trial count updated to 3 for all users');
+
+    return {success: true};
+  } catch (error) {
+    console.error('Error updating trial count:', error);
+    return {success: false};
+  }
+}
